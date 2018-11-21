a280f6b84c3a7ae55456838a5d3082e10ec5b469
Yigit Sen <senyig.20@robcol.k12.tr>
2018-11-21 21:32:39 +0300

    Create Assignment11.py

---
+++ Assignment11.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+from std_msgs.msg import Float64MultiArray, Int64MultiArray
+dataArray = []
+
+def arr0(data):
+    dataArray[0] = math.radians(data.data)
+def arr1(data):
+    dataArray[1] = math.radians(data.data)
+def arr2(data):
+    dataArray[2] = math.radians(data.data)
+def arr3(data):
+    dataArray[3] = math.radians(data.data)
+def arr4(data):
+    dataArray[4] = math.radians(data.data)
+def arr5(data):
+    dataArray[5] = math.radians(data.data)
+
+
+
+def convert():
+    rospy.init_node('converter', anonymous=True)
+    initSubs()
+    pub=rospy.Publisher('/standing2/joint_impedance_controller/position', Float64MultiArray, queue_size=10)
+    rate=rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        pub.publish(dataArray)
+        rate.sleep()
+
+def initSubs():
+    rospy.Subscriber("/standing2middleMan/J1Angle", Int64, arr0)
+    rospy.Subscriber("/standing2middleMan/J2Angle", Int64, arr1)
+    rospy.Subscriber("/standing2middleMan/J3Angle", Int64, arr2)
+    rospy.Subscriber("/standing2middleMan/J4Angle", Int64, arr3)
+    rospy.Subscriber("/standing2middleMan/J5Angle", Int64, arr4)
+    rospy.Subscriber("/standing2middleMan/J6Angle", Int64, arr5)
+
+
+
+if __name__ == '__main__':
+    try:
+        convert()
+    except rospy.ROSInterruptException:
+        pass
